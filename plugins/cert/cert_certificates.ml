c9c60a2af8d184e531586fdbfe97d0f091546347
Quentin Garchery <garchery.quentin@gmail.com>
2020-02-12 19:22:28 +0100

    original certificate uses terms and prsymbols

--- plugins/cert/cert_certificates.ml
+++ plugins/cert/cert_certificates.ml
@@ -1,6 +1,7 @@
 open Format
 
 open Why3
+open Decl
 open Term
 open Ident
 
@@ -11,58 +12,61 @@ type path = dir list
 
 (** We equip existing transformations with a certificate <certif> *)
 
-type 'a cert =
+type ('a, 'b) cert = (* 'a is used to designate an hypothesis, 'b is used for terms *)
 (* Replaying a certif <cert> against a ctask <cta> will be denoted <cert ⇓ cta>.
    For more details, take a look at the OCaml implementation <Cert_verif_caml.ccheck>. *)
   | Nc
   (* Makes verification fail : use it as a placeholder *)
   | Hole
   (* Hole ⇓ (Γ ⊢ Δ) ≜  [Γ ⊢ Δ] *)
-  | Axiom of ident * ident
+  | Cut of 'a * 'b * ('a, 'b) cert * ('a, 'b) cert
+  (* Cut (I, A, c₁, c₂) ⇓ (Γ ⊢ Δ) ≜  (c₁ ⇓ (Γ ⊢ Δ, I : A))  @  (c₂ ⇓ (Γ, I : A ⊢ Δ)) *)
+  | Let of 'b * 'a * ('a, 'b) cert
+  | Axiom of 'a * 'a
   (* Axiom (H, G) ⇓ (Γ, H : A ⊢ Δ, G : A) ≜  [] *)
-  | Trivial of ident
+  | Trivial of 'a
   (* Trivial I ⇓ (Γ, I : false ⊢ Δ) ≜  [] *)
   (* Trivial I ⇓ (Γ ⊢ Δ, I : true ) ≜  [] *)
-  | Cut of ident * 'a * 'a cert * 'a cert
-  (* Cut (I, A, c₁, c₂) ⇓ (Γ ⊢ Δ) ≜  (c₁ ⇓ (Γ ⊢ Δ, I : A))  @  (c₂ ⇓ (Γ, I : A ⊢ Δ)) *)
-  | Split of ident * 'a cert * 'a cert
+  | Split of 'a * ('a, 'b) cert * ('a, 'b) cert
   (* Split (I, c₁, c₂) ⇓ (Γ, I : A ∨ B ⊢ Δ) ≜  (c₁ ⇓ (Γ, I : A ⊢ Δ))  @  (c₂ ⇓ (Γ, I : B ⊢ Δ)) *)
   (* Split (I, c₁, c₂) ⇓ (Γ ⊢ Δ, I : A ∧ B) ≜  (c₁ ⇓ (Γ ⊢ Δ, I : A))  @  (c₂ ⇓ (Γ ⊢ Δ, I : B)) *)
-  | Unfold of ident * 'a cert
+  | Unfold of 'a * ('a, 'b) cert
   (* Unfold (I, c) ⇓ (Γ, I : A ↔ B ⊢ Δ) ≜  c ⇓ (Γ, I : (A → B) ∧ (B → A) ⊢ Δ) *)
   (* Unfold (I, c) ⇓ (Γ ⊢ Δ, I : A ↔ B) ≜  c ⇓ (Γ ⊢ Δ, I : (A → B) ∧ (B → A)) *)
   (* Unfold (I, c) ⇓ (Γ, I : A → B ⊢ Δ) ≜  c ⇓ (Γ, I : ¬A ∨ B ⊢ Δ)*)
   (* Unfold (I, c) ⇓ (Γ ⊢ Δ, I : A → B) ≜  c ⇓ (Γ ⊢ Δ, I : ¬A ∨ B)*)
-  | Swap_neg of ident * 'a cert
+  | Swap_neg of 'a * ('a, 'b) cert
   (* Swap_neg (I, c) ⇓ (Γ, I : ¬A ⊢ Δ) ≜  c ⇓ (Γ ⊢ Δ, I : A)  *)
   (* Swap_neg (I, c) ⇓ (Γ, I : A ⊢ Δ ) ≜  c ⇓ (Γ ⊢ Δ, I : ¬A) *)
   (* Swap_neg (I, c) ⇓ (Γ ⊢ Δ, I : A ) ≜  c ⇓ (Γ, I : ¬A ⊢ Δ) *)
   (* Swap_neg (I, c) ⇓ (Γ ⊢ Δ, I : ¬A) ≜  c ⇓ (Γ, I : A ⊢ Δ)  *)
-  | Destruct of ident * ident * ident * 'a cert
+  | Destruct of 'a * 'a * 'a * ('a, 'b) cert
   (* Destruct (I, J₁, J₂, c) ⇓ (Γ, I : A ∧ B ⊢ Δ) ≜  c ⇓ (Γ, J₁ : A, J₂ : B ⊢ Δ) *)
   (* Destruct (I, J₁, J₂, c) ⇓ (Γ ⊢ Δ, I : A ∨ B) ≜  c ⇓ (Γ ⊢ Δ, J₁ : A, J₂ : B) *)
-  | Construct of ident * ident * ident * 'a cert (* should be derivable from Cut and Split *)
-  (* Construct (I₁, I₂, J, c) ⇓ (Γ ⊢ Δ, I₁ : A, I₂ : B) ≜ c ⇓ (Γ ⊢ Δ, J : A ∧ B) *)
-  (* Construct (I₁, I₂, J, c) ⇓ (Γ, I₁ : A, I₂ : B ⊢ Δ) ≜ c ⇓ (Γ, J : A ∧ B ⊢ Δ) *)
-  | Weakening of ident * 'a cert
+  (* | Construct of ident * ident * ident * 'a cert (\* should be derivable from Cut and Split *\)
+   * (\* Construct (I₁, I₂, J, c) ⇓ (Γ ⊢ Δ, I₁ : A, I₂ : B) ≜ c ⇓ (Γ ⊢ Δ, J : A ∧ B) *\)
+   * (\* Construct (I₁, I₂, J, c) ⇓ (Γ, I₁ : A, I₂ : B ⊢ Δ) ≜ c ⇓ (Γ, J : A ∧ B ⊢ Δ) *\) *)
+  | Weakening of 'a * ('a, 'b) cert
   (* Weakening (I, c) ⇓ (Γ ⊢ Δ, I : A) ≜  c ⇓ (Γ ⊢ Δ) *)
   (* Weakening (I, c) ⇓ (Γ, I : A ⊢ Δ) ≜  c ⇓ (Γ ⊢ Δ) *)
-  | Intro_quant of ident * ident * 'a cert
+  | Intro_quant of 'a * ident * ('a, 'b) cert
   (* Intro_quant (I, y, c) ⇓ (Γ, I : ∃ x. P x ⊢ Δ) ≜  c ⇓ (Γ, I : P y ⊢ Δ) (y fresh) *)
   (* Intro_quant (I, y, c) ⇓ (Γ ⊢ Δ, I : ∀ x. P x) ≜  c ⇓ (Γ ⊢ Δ, I : P y) (y fresh) *)
-  | Inst_quant of ident * ident * 'a * 'a cert
+  | Inst_quant of 'a * 'a * 'b * ('a, 'b) cert
   (* Inst_quant (I, J, t, c) ⇓ (Γ, I : ∀ x. P x ⊢ Δ) ≜  c ⇓ (Γ, I : ∀ x. P x, J : P t ⊢ Δ) *)
   (* Inst_quant (I, J, t, c) ⇓ (Γ ⊢ Δ, I : ∃ x. P x) ≜  c ⇓ (Γ ⊢ Δ, I : ∃ x. P x, J : P t) *)
-  | Rewrite of ident * ident * path * bool * 'a cert list
+  | Rewrite of 'a * 'a * path * bool * ('a, 'b) cert list
   (* Rewrite (I, J, path, rev, lc) ⇓ Seq is defined as follows :
      it tries to rewrite in <I> an equality that is in <J>, following the path <path>,
      <rev> indicates if it rewrites from left to right or from right to left.
      Since <H> can have premises, those are then matched against the certificates <lc> *)
 
-type certif = term cert
-type core_certif = cterm cert
+
+type certif = (prsymbol, term) cert
+type core_certif = (ident, cterm) cert
 
 type ctrans = certif ctransformation
+
 
 (** Printing of <cterm> and <ctask> : for debugging purposes *)
 
@@ -70,10 +74,15 @@ let ip = create_ident_printer []
 
 let pri fmt i =
   fprintf fmt "%s" (id_unique ip i)
-and prd fmt = function
+
+let prpr fmt pr =
+  pri fmt pr.pr_name
+
+let prd fmt = function
   | Left -> fprintf fmt "Left"
   | Right -> fprintf fmt "Right"
-and prle sep pre fmt le =
+
+let prle sep pre fmt le =
   let prl = pp_print_list ~pp_sep:(fun fmt () -> fprintf fmt sep) pre in
   fprintf fmt "[%a]" prl le
 
@@ -107,22 +116,23 @@ let rec print_certif filename cert =
 and prc fmt = function
   | Nc -> fprintf fmt "No_certif"
   | Hole -> fprintf fmt "Hole"
-  | Axiom (h, g) -> fprintf fmt "Axiom @[(%a,@ %a)@]" pri h pri g
-  | Trivial i -> fprintf fmt "Trivial %a" pri i
-  | Cut (i, a, c1, c2) -> fprintf fmt "Cut @[(%a,@ %a,@ %a,@ %a)@]" pri i pte a prc c1 prc c2
-  | Split (i, c1, c2) -> fprintf fmt "Split @[(%a,@ %a,@ %a)@]" pri i prc c1 prc c2
-  | Unfold (i, c) -> fprintf fmt "Unfold @[(%a,@ %a)@]" pri i prc c
-  | Swap_neg (i, c) -> fprintf fmt "Swap_neg @[(%a,@ %a)@]" pri i prc c
+  | Cut (i, a, c1, c2) -> fprintf fmt "Cut @[(%a,@ %a,@ %a,@ %a)@]" prpr i pte a prc c1 prc c2
+  | Let (x, i, c) -> fprintf fmt "Let @[(%a,@ %a,@ %a)@]" pte x prpr i prc c
+  | Axiom (h, g) -> fprintf fmt "Axiom @[(%a,@ %a)@]" prpr h prpr g
+  | Trivial i -> fprintf fmt "Trivial %a" prpr i
+  | Split (i, c1, c2) -> fprintf fmt "Split @[(%a,@ %a,@ %a)@]" prpr i prc c1 prc c2
+  | Unfold (i, c) -> fprintf fmt "Unfold @[(%a,@ %a)@]" prpr i prc c
+  | Swap_neg (i, c) -> fprintf fmt "Swap_neg @[(%a,@ %a)@]" prpr i prc c
   | Destruct (i, j1, j2, c) ->
-      fprintf fmt "Destruct @[(%a,@ %a,@ %a,@ %a)@]" pri i pri j1 pri j2 prc c
-  | Construct (i1, i2, j, c) ->
-      fprintf fmt "Construct @[(%a,@ %a,@ %a,@ %a)@]" pri i1 pri i2 pri j prc c
-  | Weakening (i, c) -> fprintf fmt "Weakening@ @[(%a,@ %a)@]" pri i prc c
-  | Intro_quant (i, y, c) -> fprintf fmt "Intro_quant @[(%a,@ %a,@ %a)@]" pri i pri y prc c
-  | Inst_quant (i, j, t, c) -> fprintf fmt "Inst_quant @[(%a,@ %a,@ %a,@ %a)@]" pri i pri j pte t prc c
+      fprintf fmt "Destruct @[(%a,@ %a,@ %a,@ %a)@]" prpr i prpr j1 prpr j2 prc c
+  (* | Construct (i1, i2, j, c) ->
+   *     fprintf fmt "Construct @[(%a,@ %a,@ %a,@ %a)@]" pri i1 pri i2 pri j prc c *)
+  | Weakening (i, c) -> fprintf fmt "Weakening@ @[(%a,@ %a)@]" prpr i prc c
+  | Intro_quant (i, y, c) -> fprintf fmt "Intro_quant @[(%a,@ %a,@ %a)@]" prpr i pri y prc c
+  | Inst_quant (i, j, t, c) -> fprintf fmt "Inst_quant @[(%a,@ %a,@ %a,@ %a)@]" prpr i prpr j pte t prc c
   | Rewrite (i, j, path, rev, lc) ->
       fprintf fmt "Rewrite @[(%a,@ %a,@ %a,@ %b,@ %a)@]"
-        pri i pri j (prle "; " prd) path rev (prle "; " prc) lc
+        prpr i prpr j (prle "; " prd) path rev (prle "; " prc) lc
 
 
 let prpos fmt = function
@@ -148,46 +158,31 @@ let find_ident s h cta =
       fprintf str_formatter "%s : Can't find ident %a in the task" s pri h;
       verif_failed (flush_str_formatter ())
 
-(** Utility functions of Cert_syntax types *)
+(** Utility functions on certificate, cterm and ctask *)
 
 (* Structural functions on certificates *)
-let rec map_cert fid fct c =
-  let m = map_cert fid fct in
-  match c with
-  | Nc | Hole -> c
-  | Trivial i -> Trivial (fid i)
+let propagate f fid fte = function
+  | (Hole | Nc)  as c -> c
   | Axiom (h, g) -> Axiom (fid h, fid g)
-  | Cut (i, a, c1, c2) -> Cut (fid i, fct a, m c1, m c2)
-  | Split (i, c1, c2) -> Split (fid i, m c1, m c2)
-  | Unfold (i, c) -> Unfold (fid i, m c)
-  | Swap_neg (i, c) -> Swap_neg (fid i, m c)
-  | Destruct (i, j1, j2, c) -> Destruct (fid i, fid j1, fid j2, m c)
-  | Construct (i1, i2, j, c) -> Construct (fid i1, fid i2, fid j, m c)
-  | Weakening (i, c) -> Weakening (fid i, m c)
-  | Intro_quant (i, y, c) -> Intro_quant (fid i, fid y, m c)
-  | Inst_quant (i, j, ct, c) -> Inst_quant (fid i, fid j, fct ct, m c)
-  | Rewrite (i, j, p, b, cl) -> Rewrite (fid i, fid j, p, b, List.map m cl)
-
-let propagate f g (c : 'a cert) = match c with
-  | (Hole | Nc | Axiom _ | Trivial _) as c -> c
-  | Unfold (i, c) -> Unfold (i, f c)
+  | Trivial i -> Trivial (fid i)
   | Cut (i, a, c1, c2) ->
       let f1 = f c1 in let f2 = f c2 in
-      Cut (i, g a, f1, f2)
+      Cut (fid i, fte a, f1, f2)
+  | Let (x, i, c) -> Let (fte x, fid i, f c)
+  | Unfold (i, c) -> Unfold (fid i, f c)
   | Split (i, c1, c2) ->
       let f1 = f c1 in let f2 = f c2 in
-      Split (i, f1, f2)
-  | Swap_neg (i, c) -> Swap_neg (i, f c)
-  | Destruct (i, j1, j2, c) -> Destruct (i, j1, j2, f c)
-  | Construct (i1, i2, j, c) -> Construct (i1, i2, j, f c)
-  | Weakening (i, c) -> Weakening (i, f c)
-  | Intro_quant (i, y, c) -> Intro_quant (i, y, f c)
-  | Inst_quant (i, j, t, c) -> Inst_quant (i, j, g t, f c)
-  | Rewrite (i, j, path, rev, lc) -> Rewrite (i, j, path, rev, List.map f lc)
+      Split (fid i, f1, f2)
+  | Swap_neg (i, c) -> Swap_neg (fid i, f c)
+  | Destruct (i, j1, j2, c) -> Destruct (fid i, fid j1, fid j2, f c)
+  | Weakening (i, c) -> Weakening (fid i, f c)
+  | Intro_quant (i, y, c) -> Intro_quant (fid i, y, f c)
+  | Inst_quant (i, j, t, c) -> Inst_quant (fid i, fid j, fte t, f c)
+  | Rewrite (i, j, path, rev, lc) -> Rewrite (fid i, fid j, path, rev, List.map f lc)
 
 let rec fill stream = function
   | Hole -> Stream.next stream
-  | c -> propagate (fill stream) (fun t -> t) c
+  | c -> propagate (fill stream) (fun t -> t) (fun t -> t) c
 
 let (|>>) c1 c2 =
   let c2_stream = Stream.from (fun _ -> Some c2) in
@@ -196,6 +191,18 @@ let (|>>) c1 c2 =
 let (|>>>) c1 lc2 =
   let lc2_stream = Stream.of_list lc2 in
   fill lc2_stream c1
+
+let new_let_var str =
+  let ls = create_lsymbol (id_fresh str) [] None in
+  t_app ls [] None
+
+let construct i1 i2 j c =
+  let ti1 = new_let_var "i1" in
+  let ti2 = new_let_var "i2" in
+  Let (ti1, i1, Hole)
+  |>> Let (ti2, i2, Hole)
+  |>> Cut (j, t_and ti1 ti2, c, Hole)
+
 
 
 (* Equality *)
@@ -267,6 +274,9 @@ let rec ct_fv_subst z u t = match t with
   | CTnot t -> CTnot (ct_fv_subst z u t)
   | CTbvar _ | CTtrue | CTfalse -> t
 
+let ct_subst ct m =
+  Mid.fold ct_fv_subst ct m
+
 (* Variable closing *)
 let rec ct_fv_close x k t = match t with
   | CTbvar _ | CTtrue | CTfalse-> t
@@ -298,6 +308,7 @@ let rec mem_cont x t cont = match t with
 
 let mem x t = mem_cont x t (fun x -> x)
 
+
 (* Separates hypotheses and goals *)
 let split_cta cta =
   let open Mid in
@@ -312,5 +323,91 @@ let set_goal : ctask -> cterm -> ctask = fun cta ->
   let hg, _ = Mid.choose mg in
   fun ct -> Mid.add hg (ct, true) mh
 
-let rec make_core init_t res_t c =
-  propagate (make_core init_t res_t) abstract_term c
+let rec abstract_types c =
+    propagate abstract_types (fun pr -> pr.pr_name) abstract_term c
+
+let rec eliminate_let (cta : ctask) (m : cterm Mid.t) = function
+    | (Nc | Hole | Axiom _ | Trivial _) as c -> c
+    | Cut (i, a, c1, c2) ->
+        let cta1 = Mid.add i (a, true) cta in
+        let cta2 = Mid.add i (a, false) cta in
+        let c1 = eliminate_let cta1 m c1 in
+        let c2 = eliminate_let cta2 m c2 in
+        let a = ct_subst m a in
+        Cut (i, a, c1, c2)
+    | Let (x, i, c) ->
+        let x = match x with
+                | CTfvar i -> i
+                | _ -> failwith "" in
+        let a, _ = Mid.find i cta in
+        let m = Mid.add x a m in
+        eliminate_let cta m c
+    | Split (i, c1, c2) ->
+        let t, pos = find_ident "split" i cta in
+        let cta1, cta2 = match t, pos with
+          | CTbinop (Tand, t1, t2), true | CTbinop (Tor, t1, t2), false ->
+              let cta1 = Mid.add i (t1, pos) cta in
+              let cta2 = Mid.add i (t2, pos) cta in
+              cta1, cta2
+          | _ -> verif_failed "Not splittable" in
+        let c1 = eliminate_let cta1 m c1 in
+        let c2 = eliminate_let cta2 m c2 in
+        Split (i, c1, c2)
+    | Unfold (i, c) ->
+        let t, pos = find_ident "unfold" i cta in
+        let cta =  match t with
+          | CTbinop (Tiff, t1, t2) ->
+              let imp_pos = CTbinop (Timplies, t1, t2) in
+              let imp_neg = CTbinop (Timplies, t2, t1) in
+              let unfolded_iff = CTbinop (Tand, imp_pos, imp_neg), pos in
+              Mid.add i unfolded_iff cta
+          | CTbinop (Timplies, t1, t2) ->
+              let unfolded_imp = CTbinop (Tor, CTnot t1, t2), pos in
+              Mid.add i unfolded_imp cta
+          | _ -> verif_failed "Nothing to unfold" in
+        let c = eliminate_let cta m c in
+        Unfold (i, c)
+    | Swap_neg (i, c) ->
+        let t, pos = find_ident "swap_neg" i cta in
+        let neg_t = match t with CTnot t -> t | t -> CTnot t in
+        let cta = Mid.add i (neg_t, not pos) cta in
+        let c = eliminate_let cta m c in
+        Swap_neg (i, c)
+    | Destruct (i, j1, j2, c) ->
+        let t, pos = find_ident "destruct" i cta in
+        let cta = match t, pos with
+          | CTbinop (Tand, t1, t2), false | CTbinop (Tor, t1, t2), true ->
+              Mid.remove i cta
+              |> Mid.add j1 (t1, pos)
+              |> Mid.add j2 (t2, pos)
+          | _ -> verif_failed "Nothing to destruct" in
+        let c = eliminate_let cta m c in
+        Destruct (i, j1, j2, c)
+    | Weakening (i, c) ->
+        let cta = Mid.remove i cta in
+        let c = eliminate_let cta m c in
+        Weakening (i, c)
+    | Intro_quant (i, y, c) ->
+        let t, pos = find_ident "intro_quant" i cta in
+        let cta = match t, pos with
+          | CTquant (CTforall, t), true | CTquant (CTexists, t), false ->
+              if mem y t then verif_failed "non-free variable" else
+                Mid.add i (ct_open t (CTfvar y), pos) cta
+          | _ -> verif_failed "Nothing to introduce" in
+        let c = eliminate_let cta m c in
+        Intro_quant (i, y, c)
+    | Inst_quant (i, j, t_inst, c) ->
+        let t, pos = find_ident "inst_quant" i cta in
+        let cta = match t, pos with
+          | CTquant (CTforall, t), false | CTquant (CTexists, t), true ->
+              Mid.add j (ct_open t t_inst, pos) cta
+          | _ -> verif_failed "trying to instantiate a non-quantified hypothesis" in
+        let c = eliminate_let cta m c in
+        Inst_quant (i, j, t_inst, c)
+    | Rewrite (i, j, path, rev, lc) as c -> c
+                                         (* TODO *)
+        (* let lcta = check_rewrite cta rev j i [] path in
+         * List.map2 ccheck lc lcta |> List.concat *)
+
+let make_core init_ct _ c =
+  eliminate_let init_ct Mid.empty (abstract_types c)
