030eee7f05348134919b5e6bd5b529e3e14d9121
Quentin Garchery <garchery.quentin@gmail.com>
2020-10-29 14:21:29 +0100

    cleaning and improving Lamdapi printing

--- plugins/cert/cert_verif_lambdapi.ml
+++ plugins/cert/cert_verif_lambdapi.ml
@@ -5,184 +5,185 @@ open Format
 open Cert_abstract
 open Cert_certificates
 
-let find_goal cta =
-  let _, (t, _) = Mid.(filter (fun _ (_, b) -> b) cta |> choose) in
-  t
-
-(* We represent a ctask <H₁ : A₁, ..., Hₘ : Aₘ ⊢ G₁ : B₁, ..., Gₘ : Bₘ >
-   by the formula <A₁ → ... → Aₘ → ¬B₁ → ... → ¬Bₙ → ⊥ >
-   As an intermediate data structure, we use a list in order to :
-     • fix the order
-     • remember the idents which are used to construct the proof term
+(* We represent a ctask
+   x₁ : ty₁,.., xᵢ : tyᵢ | H₁ : A₁,.., Hⱼ : Aⱼ ⊢ G₁ : B₁, ..., Gₖ : Bₖ
+   by the formula
+   ∀ x₁ : ty₁, ... ∀ xᵢ: tyᵢ, A₁ → ... → Aⱼ → ¬B₁ → ... → ¬Bₖ → ⊥
+   As an intermediate data structure, we use lists in order to fix the order
+   and remember the idents which are used to construct the proof term.
  *)
-type ctask_simple = (ident * cterm) list
+type ctask_simple =
+  { s  : (ident * ctype) list;
+    gd : (ident * cterm) list }
 
+let simplify_task (cta : ctask) : ctask_simple =
+  let encode_neg (k, (ct, pos)) = k, if pos then CTnot ct else ct in
+  { s = Mid.bindings cta.sigma;
+    gd = Mid.bindings cta.gamma_delta
+         |> List.map encode_neg }
 
-(* on [e1; ...; en], print_list sep gives :
-   e1 sep e2 sep ... en sep
- *)
-let print_list sep pe fmt l =
-  List.iter (fun h -> fprintf fmt "%a%s" pe h sep) l
-
-let print_list_inter sep pe fmt l =
-  pp_print_list ~pp_sep:(fun fmt () -> fprintf fmt "%s" sep)
-    pe fmt l
-
-(* on [e1; ...; en], print_list_pre gives :
-   sep (e1) (sep (e2) ...)
- *)
-let rec print_list_pre sep pe fmt = function
-  | [] -> failwith "cannot print empty list with preorder"
-  | [x] -> pe fmt x
-  | h::t -> fprintf fmt "%s (%a) (%a)"
-              sep
-              pe h
-              (print_list_pre sep pe) t
-
-(* let rec collect typ ct = match ct with
- *   | CTint _ | CTbvar _  -> Mid.empty
- *   | CTfvar id -> Mid.singleton id typ
- *   | CTapp (ct1, ct2) -> Mid.set_union (collect (Arrow (Term, typ)) ct1) (collect Term ct2)
- *   | CTbinop (_, ct1, ct2) -> Mid.set_union (collect typ ct1) (collect typ ct2)
- *   | CTquant (_, ct)
- *   | CTnot ct -> collect typ ct
- *   | CTtrue | CTfalse -> Mid.empty
- *
- * let collect_stask (ta : ctask_simple) =
- *   List.fold_left (fun acc (_, ct) -> Mid.set_union acc (collect Prop ct))
- *     Mid.empty ta *)
-
-let print_task fmt (fv, ts) =
+let print_task fmt {s; gd} =
   fprintf fmt "@[<hov 3>(Π ";
-  pp_print_list ~pp_sep:(fun fmt () -> fprintf fmt "@ ")
+  pp_print_list ~pp_sep:pp_print_space
     (fun fmt (id, cty) ->
       fprintf fmt "(%a : %a)"
         pri id
-        prty cty) fmt fv;
-  let tp = snd (List.split ts) @ [CTfalse] in
+        prty cty) fmt s;
+  let _, terms = List.split gd in
+  let tp = terms @ [CTfalse] in
   fprintf fmt ",@]@  @[<hv 7>etype (";
   pp_print_list ~pp_sep:(fun fmt () -> fprintf fmt " ⇨@ ") prdisj fmt tp;
   fprintf fmt ")@])"
 
-let rstr goal = if goal then "_goal" else "_hyp"
-
 let print_certif at fmt c =
   let s = Stream.of_list at in
+  let rstr goal = if goal then "_goal" else "_hyp" in
   let rec pc fmt = function
   | ELet _ | EConstruct _ | ERename _ | EFoldArr _ ->
       verif_failed "Construct/Let/Rename/Fold left"
   | EHole _ ->
       fprintf fmt "%s" (Stream.next s)
   | EAxiom (t, h, g) ->
-      fprintf fmt "axiom (%a) %a %a"
-        pcte t
+      fprintf fmt "axiom %a %a %a"
+        prpv t
         pri h
         pri g
   | ETrivial (goal, g) ->
-      fprintf fmt "trivial%s %a" (rstr goal)
+      fprintf fmt "trivial%s %a"
+        (rstr goal)
         pri g
   | ECut (i, a, ce1, ce2) ->
-      fprintf fmt "cut (%a) (λ %a, %a) (λ %a, %a)"
-        pcte a
+      fprintf fmt "cut %a@ \
+                   (λ %a, @[<hv 0>%a@])@ \
+                   (λ %a, @[<hv 0>%a@])"
+        prpv a
         pri i pc ce1
         pri i pc ce2
   | ESplit (goal, a, b, i, c1, c2) ->
-      fprintf fmt "split%s (%a) (%a) (λ %a, %a) (λ %a, %a) %a" (rstr goal)
-        pcte a
-        pcte b
+      fprintf fmt "split%s %a %a@ \
+                   (λ %a, @[<hv 0>%a@])@ \
+                   (λ %a, @[<hv 0>%a@])@ \
+                   %a"
+        (rstr goal)
+        prpv a
+        prpv b
         pri i pc c1
         pri i pc c2
         pri i
   | EUnfoldIff (goal, a, b, i, c) ->
-      fprintf fmt "unfold_iff%s (%a) (%a) (λ %a, %a) %a" (rstr goal)
-        pcte a
-        pcte b
+      fprintf fmt "unfold_iff%s %a %a@ \
+                   (λ %a, @[<hv 0>%a@])@ \
+                   %a"
+        (rstr goal)
+        prpv a
+        prpv b
         pri i pc c
         pri i
   | EUnfoldArr (goal, a, b, i, c) ->
-      fprintf fmt "unfold_arr%s (%a) (%a) (λ %a, %a) %a" (rstr goal)
-        pcte a
-        pcte b
+      fprintf fmt "unfold_arr%s %a %a@ \
+                   (λ %a, @[<hv 0>%a@])@ \
+                   %a"
+        (rstr goal)
+        prpv a
+        prpv b
         pri i pc c
         pri i
   | ESwapNeg (goal, a, i, c) ->
-      fprintf fmt "swap_neg%s (%a) (λ %a, %a) %a" (rstr goal)
-        pcte a
+      fprintf fmt "swap_neg%s %a@ \
+                   (λ %a, @[<hv 0>%a@])@ \
+                   %a"
+        (rstr goal)
+        prpv a
         pri i pc c
         pri i
   | ESwap (goal, a, i, c) ->
-      fprintf fmt "swap%s (%a) (λ %a, %a) %a" (rstr goal)
-        pcte a
+      fprintf fmt "swap%s %a@ \
+                   (λ %a, @[<hv 0>%a@])@ \
+                   %a"
+        (rstr goal)
+        prpv a
         pri i pc c
         pri i
   | EDestruct (goal, a, b, i, j1, j2, c) ->
-      fprintf fmt "destruct%s (%a) (%a) (λ %a %a, %a) %a" (rstr goal)
-        pcte a
-        pcte b
+      fprintf fmt "destruct%s %a %a@ \
+                   (λ %a %a, @[<hv 0>%a@])@ \
+                   %a"
+        (rstr goal)
+        prpv a
+        prpv b
         pri j1 pri j2 pc c
         pri i
   | EWeakening (goal, a, i, c) ->
-      fprintf fmt "weakening%s (%a) (%a) %a" (rstr goal)
-        pcte a
+      fprintf fmt "weakening%s %a@ \
+                   (@[<hv 0>%a@])@ \
+                   %a"
+        (rstr goal)
+        prpv a
         pc c
         pri i
   | EIntroQuant (goal, p, i, y, c) ->
-      fprintf fmt "intro_quant%s (%a) (λ %a %a, %a) %a" (rstr goal)
-        pcte p
+      fprintf fmt "intro_quant%s %a@ \
+                   (λ %a %a, @[<hv 0>%a@])@ \
+                   %a"
+        (rstr goal)
+        prpv p
         pri y pri i pc c
         pri i
   | EInstQuant (goal, p, i, j, t, c) ->
-      fprintf fmt "inst_quant%s (%a) (%a) (λ %a %a, %a) %a" (rstr goal)
-        pcte p
-        pcte t
+      fprintf fmt "inst_quant%s %a %a@ \
+                   (λ %a %a, @[<hv 0>%a@])@ \
+                   %a"
+        (rstr goal)
+        prpv p
+        prpv t
         pri i pri j pc c
         pri i
-  | ERewrite _ -> verif_failed "Rewrite is not yet supported by the Lambdapi checker" in
+  | ERewrite _ -> verif_failed "Rewrite is not yet supported by the Lambdapi checker"
+  in
   pc fmt c
 
-let fv_ts (cta : ctask) =
-  let encode_neg (k, (ct, pos)) = k, if pos then CTnot ct else ct in
-  let fv = Mid.bindings cta.sigma in
-  let ts = Mid.bindings cta.gamma_delta
-           |> List.map encode_neg in
-  fv, ts
+let pp_print_blank fmt () =
+  fprintf fmt " "
 
-let print fmt init_ct res_ct (task_id, certif) =
-  let res = List.map fv_ts res_ct in
-  let init = fv_ts init_ct in
+let print fmt init res (task_ids, certif) =
+  let res = List.map simplify_task res in
+  let init = simplify_task init in
   (* The type we need to check is inhabited *)
   let p_type fmt () =
     pp_print_list ~pp_sep:(fun fmt () -> fprintf fmt " →@ ")
       print_task fmt (res @ [init]) in
   (* applied_tasks are used to fill the holes *)
   let applied_tasks =
-    List.map2 (fun id (fv_t, t) ->
-        let fv, _ = List.split fv_t in
-        let hyp, _ = List.split t in
-        print_list_inter " " pri str_formatter (id :: fv @ hyp);
+    List.map2 (fun task_id {s; gd} ->
+        let fv_ids, _ = List.split s in
+        let hyp_ids, _ = List.split gd in
+        pp_print_list ~pp_sep:pp_print_blank
+          pri str_formatter (task_id :: fv_ids @ hyp_ids);
         flush_str_formatter ())
-      task_id
+      task_ids
       res in
   (* The term that has the correct type *)
   let p_term fmt () =
-    let fv, ts = init in
-    let fv_ids, _ = List.split fv in
-    let hyp_ids, _ = List.split ts in
-    let vars = task_id @ fv_ids @ hyp_ids in
-    fprintf fmt "λ %a, " (print_list_inter " " pri) vars;
+    let {s; gd} = init in
+    let fv_ids, _ = List.split s in
+    let hyp_ids, _ = List.split gd in
+    let vars = task_ids @ fv_ids @ hyp_ids in
+    fprintf fmt "λ ";
+    pp_print_list ~pp_sep:pp_print_blank pri fmt vars;
+    fprintf fmt ",@ ";
     print_certif applied_tasks fmt certif in
 
   fprintf fmt "@[<v 0>definition to_verify :@   \
                @[<v 0>%a@]@ \
-               ≔  %a@]@."
+               ≔  @[<v 0>%a@]@]@."
     p_type ()
     p_term ()
 
-let checker_lambdapi certif init_ct res_ct =
+let checker_lambdapi certif init res =
   try
     let oc = open_out "/tmp/check_line.lp" in
     let fmt = formatter_of_out_channel oc in
-    print fmt init_ct res_ct certif;
+    print fmt init res certif;
     close_out oc;
     let coc = Filename.(concat Config.datadir (concat "lambdapi" "CoC.lp")) in
     let pkg_conf = Filename.(concat Config.datadir (concat "lambdapi" "lambdapi.pkg")) in
