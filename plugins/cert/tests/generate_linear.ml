ee7e1e7982b3913b07042c70612f6fd2ecf4a9fd
Quentin Garchery <quentin.garchery@inria.fr>
2021-04-16 02:41:44 +0200

    folder tests and some cleaning

---
+++ plugins/cert/tests/generate_linear.ml
@@ -0,0 +1,25 @@
+open Format
+
+let rec print_pred i fmt n =
+  if i <= n then begin
+      fprintf fmt "predicate p%d@ " i;
+      print_pred (i+1) fmt n
+    end
+
+let rec print_goal i fmt n =
+  if i < n then begin
+      fprintf fmt "(p%d -> p%d) ->@ " i (i+1);
+      print_goal (i+1) fmt n
+    end
+
+
+let _ =
+  let n = Sys.argv.(1) |> int_of_string in
+  let filename = sprintf "linear%d.mlw" n in
+  let oc = open_out filename in
+  let fmt = formatter_of_out_channel oc in
+  fprintf fmt "@[<v>%a @ @]goal G : @[p1 ->@ %ap%d@]@."
+          (print_pred 1) n
+          (print_goal 1) n
+          n;
+  close_out oc
