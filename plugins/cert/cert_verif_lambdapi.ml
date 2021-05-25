f9ba3ea359629b3213524b929ae17c686ba58061
Quentin Garchery <quentin.garchery@inria.fr>
2021-05-25 13:14:34 +0200

    remove unnecessary unification rules

--- plugins/cert/cert_verif_lambdapi.ml
+++ plugins/cert/cert_verif_lambdapi.ml
@@ -157,8 +157,6 @@ let print fmt init res certif =
     print_certif fmt certif in
 
   fprintf fmt "@[<v>require open cert_lambdapi.preamble;@ @ \
-               unif_rule $a → $b ≡ kEv $c ↪ [ $a ≡ tEv $a'; $b ≡ kEv $b'; $c ≡ $a' ⇁ $b' ];@ \
-               unif_rule $a → $b ≡ tEv $c ↪ [ $a ≡ tEv $a'; $b ≡ tEv $b'; $c ≡ $a' ⇒ $b' ];@ @ \
                symbol to_verify :@ \
                @[<v>%a@]@ \
                @<3>%s@[<v>%a@]@];@."
