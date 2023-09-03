23495ff6d9f4b169ad61adf3a518026c8b6f153d
Andrei Paskevich <andrei@lri.fr>
2023-09-03 12:39:50 +0200

    add coma plugin

--- plugins/coma/coma_main.ml
+++ plugins/coma/coma_main.ml
@@ -11,11 +11,13 @@
 
 open Why3
 
-let read_channel _env _path file c =
-  let ast = Mome_lexer.parse file c in
+let read_channel _env _path _file _c =
+(*
+  let ast = Coma_lexer.parse file c in
   Format.printf "@[%a@]@." (fun fmt l ->
-    List.iter (fun d -> Mome_syntax.print_decl fmt d) l) ast;
+    List.iter (fun d -> Coma_syntax.print_decl fmt d) l) ast;
+*)
   Wstdlib.Mstr.empty
 
-let () = Env.register_format Env.base_language "mome" ["mome"] read_channel
-  ~desc:"Memory Model language"
+let () = Env.register_format Env.base_language "coma" ["coma"] read_channel
+  ~desc:"Continuation Machine language"
